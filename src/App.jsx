25de0b4aef57162859aa24a2c301d5e00b3a8acb
MICHELINEOKITO <135968565+MICHELINEOKITO@users.noreply.github.com>
2023-11-21 09:52:24 +0000

    import bibi image
    Essaie de commit avec bibi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/home.jsx";
 import Layout from "./components/layout.jsx";
+import bibi from"./images/bibi.jpg";
 
 import('./style/reset.css');
 import('./style/App.css');
 
 export default function App() {
-  return (
+  return ( 
     <Layout>
       <Home />
     </Layout>
