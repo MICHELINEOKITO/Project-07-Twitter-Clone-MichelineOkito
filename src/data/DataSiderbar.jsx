fad5a93b096aafc4a8fba0daa204042404087b28
Micheline-Ok <okitobibi2@gmail.com>
2023-12-14 17:58:23 +0100

    Add icons text

---
+++ src/data/DataSiderbar.jsx
@@ -0,0 +1,47 @@
+const DataSiderbar = [
+    {
+        name: 'Home',
+        icone: 'src/images/Home-Fill.svg',
+        path: '/'
+    },
+
+    {
+        name: 'Explore',
+        icone: 'src/images/Explore.svg',
+        path: '/explore'
+    },
+    {
+        name: 'Notifications',
+        icone: 'src/images/Notifications.svg',
+        path: '/notifications'
+      
+    },
+    {
+        name: 'Messages',
+        icone: 'src/images/Messages.svg',
+        path: '/messages'
+    },
+       
+    {
+        name: 'Bookmarks',
+        icone: 'src/images/Bookmarks.svg',
+        path: '/bookmarks'
+    },
+    {
+        name: 'Lists',
+        icone: 'src/images/Lists.svg',
+        path: '/lists'
+    },
+    {
+        name: 'Profile',
+        icone: 'src/images/Profile.svg',
+        path: '/profile'
+    },
+    {
+        name: 'More',
+        icone: 'src/images/More.svg',
+        path: '/more'
+    }
+
+]
+export default DataSiderbar;
