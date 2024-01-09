0885dd5bac163100c7b6087c9fe09c5da6b9ca71
Micheline-Ok <okitobibi2@gmail.com>
2024-01-09 12:29:42 +0100

    creation of different data files

---
+++ src/components/profile-button.jsx
@@ -0,0 +1,24 @@
+
+
+
+const ProfileButton = () =>{
+   
+
+         return (
+            <button className="profile-button">
+                <div className="text-content">
+                <div className="text">
+                  <div>Micheline Okito</div>
+                  <div>okitobibi2@gmail.com</div>
+                </div>
+                <div>
+                  ...
+                </div>
+                </div>
+                
+            </button>
+          );
+        };
+
+
+export default ProfileButton;
---
+++ src/data/DataWhoToFollow.jsx
@@ -0,0 +1,23 @@
+const DataWhoToFollow =[
+    {
+        id:1,
+        image:"src/images/Tweet-Profile-Photo new york.svg",
+        title:"Trebding in Turkey",
+        author:"@nytimes"
+     },
+     {
+         id:2,
+         image:"src/images/Tweet-Profile-Photo.svg",
+         title:"Trebding in Turkey",
+         author:"@CNN"
+      },
+      {
+         id:3,
+         image:"src/images/Tweeter.svg",
+         title:"Trebding in Turkey",
+         author:"@Twitter"
+      }
+
+
+]
+export default DataWhoToFollow;
