ac5b83f4a1d9afc3f37b25da42cb73ad1a3980b2
Micheline-Ok <okitobibi2@gmail.com>
2024-01-13 14:37:03 +0100

    Mis en place de champ input de trends

---
+++ src/components/Trends/Trends.jsx
@@ -0,0 +1,26 @@
+
+
+
+const Trends = () =>{
+    return(
+        <div className="trends">
+
+            <div className="trends-input">
+                <img src="src/images/search.svg" alt="" />
+                <input type="text" placeholder="Search tweeter"/>
+
+            </div>
+
+            <div className="trends-for-you">
+
+            </div>
+
+            <div className="trends-follow">
+
+            </div>
+
+        </div> 
+    )
+
+}
+export default Trends;
