fc27cd9d55db746d29ebe45fe34cb95df33a58ed
Micheline-Ok <okitobibi2@gmail.com>
2024-01-13 19:24:40 +0100

    Configuration de Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Layout from "./components/layout.jsx";
 import Home from "./pages/home.jsx";
 import('./style/reset.css');
 import('./style/App.css');
-import('./style/sidebar.css')
 
 export default function App() {
   return (
