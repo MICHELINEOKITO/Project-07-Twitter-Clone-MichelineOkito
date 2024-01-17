26670d8996007b13491aef3d0538c68cec72692e
Micheline-Ok <okitobibi2@gmail.com>
2024-01-17 13:03:29 +0100

    configuration des nouvelles routes version 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import Layout from "./components/layout.jsx";
 import Home from "./pages/home.jsx";
 import('./style/reset.css');
 import('./style/App.css');
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Layout>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home />} />
+          <Route path="/" element={ <Home />} />
+          <Route path="/" element={ <Home />} />
+          <Route path="/" element={ <Home />} />
+          <Route path="/" element={ <Home />} />
+          <Route path="/" element={ <Home />} />
+          <Route path="/" element={ <Home />} />
+        </Routes>
+      </BrowserRouter>
+     
     </Layout>
   )
   
