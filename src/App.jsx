416dfd467c9e8d69c278adb30de8036b74972ab1
Micheline okito <okitobibi2@gmail.com>
2024-03-14 09:23:30 +0100

    Filtrage des tweets sur la page de profil

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
-import Layout from "./components/layout.jsx";
-import Home from "./pages/home.jsx";
-import Explore from "./components/Explore/Explore.jsx";
-import Notifications from "./components/Notification/Notification.jsx";
-import Messages from "./components/Message/Message.jsx";
-import Bookmarks from "./components/Bookmark/Bookmarks.jsx";
-import List from "./components/List/List.jsx";
-import Profile from "./components/Profile/Profile.jsx";
-import('./style/reset.css');
-import('./style/App.css');
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./components/layout.jsx";
+// import Home from "./pages/home.jsx";
+// import Explore from "./components/Explore/Explore.jsx";
+// import Notifications from "./components/Notification/Notification.jsx";
+// import Messages from "./components/Message/Message.jsx";
+// import Bookmarks from "./components/Bookmark/Bookmarks.jsx";
+// import List from "./components/List/List.jsx";
+// import Profile from "./components/Profile/Profile.jsx";
+// import('./style/reset.css');
+// import('./style/App.css');
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
